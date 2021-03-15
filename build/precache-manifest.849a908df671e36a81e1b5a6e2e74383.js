502af729c911db36396d7ebe355abc00ab81b934
Chiara BoisAubin <chiara.boisaubin@gmail.com>
2021-03-15 06:08:01 -0400

    update content

---
+++ build/precache-manifest.849a908df671e36a81e1b5a6e2e74383.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5186fd5b6b6ccffb5045e497bcff3df0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2733834114bb4b5b1789",
+    "url": "/static/css/main.f40d6c99.chunk.css"
+  },
+  {
+    "revision": "81681d626fe00a3b3199",
+    "url": "/static/js/2.8267c13e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8267c13e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2733834114bb4b5b1789",
+    "url": "/static/js/main.34bd0e3e.chunk.js"
+  },
+  {
+    "revision": "39a082e33ff442e6da07",
+    "url": "/static/js/runtime-main.fc073587.js"
+  }
+]);
