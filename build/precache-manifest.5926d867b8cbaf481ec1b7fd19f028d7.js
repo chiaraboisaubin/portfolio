3eaffcdaf33a9e98f694a4f46938ef19e1960faf
Chiara BoisAubin <chiara.boisaubin@gmail.com>
2021-03-15 18:25:08 -0400

    Revert "Revert "content""
    This reverts commit 5fbcfc7f2cbce6bd8060fd99c1bb8ca5b2bb54b9.

---
+++ build/precache-manifest.5926d867b8cbaf481ec1b7fd19f028d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b4c26fef276355afe44b15e0f05a824",
+    "url": "/chiaraboisaubin.com/index.html"
+  },
+  {
+    "revision": "55a2490482d11640b7a9",
+    "url": "/chiaraboisaubin.com/static/css/main.f40d6c99.chunk.css"
+  },
+  {
+    "revision": "cfd08b717655c9a556f6",
+    "url": "/chiaraboisaubin.com/static/js/2.fe9100b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chiaraboisaubin.com/static/js/2.fe9100b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55a2490482d11640b7a9",
+    "url": "/chiaraboisaubin.com/static/js/main.089d04fb.chunk.js"
+  },
+  {
+    "revision": "8a415a9c43de505cc95c",
+    "url": "/chiaraboisaubin.com/static/js/runtime-main.8092527e.js"
+  }
+]);
