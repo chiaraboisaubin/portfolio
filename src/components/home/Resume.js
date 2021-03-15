8f4edac8d5e303337b3cfbce5b5a863756622cf9
Chiara BoisAubin <chiara.boisaubin@gmail.com>
2021-03-15 19:04:41 -0400

    updating content

---
+++ src/components/home/Resume.js
@@ -0,0 +1,18 @@
+
+import React, { Component } from 'react';
+export default class Resume extends Component {
+  render() {
+    return (
+      <React.Fragment>
+      {/*generated code*/}
+<a name="resume"></a>
+<div className="resume">
+<p> Want to see more of my experience? </p>
+<button className="about">  <a href="https://drive.google.com/file/d/1cYw-RW5DqSFqZoOPFQWe5UAexpyU25k4/view?usp=sharing" rel="noopener noreferrer" target="_blank"> 
+          Here's my resume </a></button>
+            </div>
+      </React.Fragment>
+    );
+  }
+}
+
---
+++ src/components/soulmarcos/HeroLayout.js
@@ -0,0 +1,15 @@
+
+
+import React, { Component } from 'react';
+export default class HeroLayout extends Component {
+  render() {
+    return (
+      <React.Fragment>
+      {/*generated code*/}
+      <div className="layout-hero">
+        <h1 className="layout-hero-header hero-header">chowdown</h1>
+    </div>
+      </React.Fragment>
+    );
+  }
+}
