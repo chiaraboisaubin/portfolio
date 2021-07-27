a0977485e3953c172284442aafecc2cd3296bff0
Chiara BoisAubin <chiara.boisaubin@gmail.com>
2021-07-26 23:49:08 -0400

    updated the UI element

---
+++ build/precache-manifest.27baaf7a3602ae808e3a16d0e6677f01.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2097b0db629fdf3dd982d5970880cf2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffbdf140ca712cff3170",
+    "url": "/static/css/2.12f074cd.chunk.css"
+  },
+  {
+    "revision": "918a76427075e0b9cae1",
+    "url": "/static/css/main.91479358.chunk.css"
+  },
+  {
+    "revision": "ffbdf140ca712cff3170",
+    "url": "/static/js/2.bd598c0a.chunk.js"
+  },
+  {
+    "revision": "1d0d605e12b1e5de496807b75f4cc1f7",
+    "url": "/static/js/2.bd598c0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "918a76427075e0b9cae1",
+    "url": "/static/js/main.bd7203f2.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "/static/media/default-skin.b257fa9c.svg"
+  }
+]);
--- src/components/careof/ContainerOne.js
+++ src/components/careof/ContainerOne.js
@@ -82,7 +82,7 @@ export default class ContaierOne extends Component {
             <img src="https://i.ibb.co/cQ8v2cK/icons.png" alt="icons" border="0"/>
            
             <h3> UI Elements </h3>
-            <img src="https://i.ibb.co/wRYtFmR/Screen-Shot-2021-05-30-at-11-56-16-PM-2.png" alt="Screen-Shot-2021-05-30-at-11-56-16-PM-2" border="0"/>
+            <img src="https://i.ibb.co/Y7FqxsP/ui-element.png" alt="ui-element" border="0"/>
 
             <h3> Color Palette </h3>
            <article> I wanted to go with colors that were a mixer of simple and neutral colors with a hint of golds for the colors. The paintings should have random patterns and sometimes transparent with one another. Going with the chosen colors was not to focus ontake away focus from the vitamins, and they are not too bright.</article> 
